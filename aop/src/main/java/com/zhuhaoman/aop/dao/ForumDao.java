e9de87ad0b8714ec99eb40ab6b8ff138a68f9a30
i01007600819 <zhuhaoman@cathay-ins.com.cn>
2019-07-29 18:45:12 +0800

    'commit'

---
+++ aop/src/main/java/com/zhuhaoman/aop/dao/ForumDao.java
@@ -0,0 +1,11 @@
+package com.zhuhaoman.aop.dao;
+
+/**
+ * @ClassName ForumDao
+ * @Description TODO
+ * @Author zhuhaoman
+ * @Date 2019/7/29 18:43
+ **/
+@Mapper
+public interface ForumDao {
+}
---
+++ aop/src/main/java/com/zhuhaoman/aop/service/BbtForum.java
@@ -0,0 +1,17 @@
+package com.zhuhaoman.aop.service;
+
+import com.zhuhaoman.aop.dao.ForumDao;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName BbtForum
+ * @Description TODO
+ * @Author zhuhaoman
+ * @Date 2019/7/29 18:42
+ **/
+@Service
+public class BbtForum {
+    public ForumDao forumDao;
+
+
+}
